1091b72657689f68badd1ae3e4f9e383227e7c94
Bernard Siebens <bernard@siebens.org>
2021-11-13 11:11:26 +0100

    Wrapped up the testing and models for the Currency & CurrencyConversion models

--- blackbook/models/currency.py
+++ blackbook/models/currency.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import Q
+from django.db.models import Q, base
 from django.utils import timezone
 
 from decimal import Decimal
@@ -19,4 +19,53 @@ class Currency(models.Model):
     def save(self, *args, **kwargs):
         self.code = self.code.upper()
 
-        super("Currency", self).save(*args, **kwargs)
+        super(Currency, self).save(*args, **kwargs)
+
+
+class CurrencyConversion(models.Model):
+    base_currency = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name="base_currency")
+    target_currency = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name="target_currency")
+    multiplier = models.DecimalField(max_digits=20, decimal_places=5)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ["timestamp"]
+
+    def __str__(self):
+        return "1 {i.base_currency} = {i.multiplier} {i.target_currency} ({timestamp})".format(
+            i=self, timestamp=self.timestamp.strftime("%d %b %Y %H:%m")
+        )
+
+    @classmethod
+    def convert(cls, base_currency, target_currency, amount):
+        conversion_object = None
+        multiplier = 1
+
+        base_currency = base_currency.upper() if type(base_currency) == str else base_currency
+        target_currency = target_currency.upper() if type(target_currency) == str else target_currency
+
+        if base_currency == target_currency:
+            return amount
+
+        try:
+            conversion_object = (
+                cls.objects.filter(
+                    Q(base_currency__code=base_currency, target_currency__code=target_currency)
+                    | Q(base_currency__code=target_currency, target_currency__code=base_currency)
+                )
+                .select_related()
+                .latest("timestamp")
+            )
+
+            if conversion_object.base_currency.code == str(base_currency):
+                multiplier = conversion_object.multiplier
+            else:
+                multiplier = 1 / conversion_object.multiplier
+
+        except cls.DoesNotExist:
+            pass
+
+        return Decimal(amount) * multiplier
+
+    def convert_to(self, target_currency, amount=1):
+        return self.__class__.convert(base_currency=self.base_currency, target_currency=target_currency, amount=amount)
--- blackbook/tests/test_currency_model.py
+++ blackbook/tests/test_currency_model.py
@@ -26,57 +26,61 @@ class CurrencyConversionTest(TestCase):
         cls.CHF = Currency.objects.create(code="CHF")
         cls.USD = Currency.objects.create(code="USD")
 
-        cls.EUR_TO_CHF = CurrencyConversion.objects.create(base=cls.EUR, target=cls.CHF, multiplier=2)
+        cls.timestamp = timezone.now()
+        cls.EUR_TO_CHF = CurrencyConversion.objects.create(base_currency=cls.EUR, target_currency=cls.CHF, multiplier=2, timestamp=cls.timestamp)
+
+    def testString(self):
+        self.assertEqual(str(self.EUR_TO_CHF), "1 EUR = 2 CHF ({timestamp})".format(timestamp=self.timestamp.strftime("%d %b %Y %H:%m")))
 
     def testConversionWithString(self):
-        eur_to_chf = CurrencyConversion.convert(base="EUR", target="CHF", amount=1)
-        chf_to_eur = CurrencyConversion.convert(base="CHF", target="EUR", amount=1)
+        eur_to_chf = CurrencyConversion.convert(base_currency="EUR", target_currency="CHF", amount=1)
+        chf_to_eur = CurrencyConversion.convert(base_currency="CHF", target_currency="EUR", amount=1)
 
         self.assertEqual(eur_to_chf, 2)
         self.assertEqual(chf_to_eur, 0.5)
 
     def testConversionWithLowercaseString(self):
-        eur_to_chf = CurrencyConversion.convert(base="eur", target="chf", amount=1)
-        chf_to_eur = CurrencyConversion.convert(base="chf", target="eur", amount=1)
+        eur_to_chf = CurrencyConversion.convert(base_currency="eur", target_currency="chf", amount=1)
+        chf_to_eur = CurrencyConversion.convert(base_currency="chf", target_currency="eur", amount=1)
 
         self.assertEqual(eur_to_chf, 2)
         self.assertEqual(chf_to_eur, 0.5)
 
     def testConversionWithSameString(self):
-        eur_to_eur = CurrencyConversion.convert(base="EUR", target="EUR", amount=1)
+        eur_to_eur = CurrencyConversion.convert(base_currency="EUR", target_currency="EUR", amount=1)
 
         self.assertEqual(eur_to_eur, 1)
 
     def testConversionWithStringUnknownCurrency(self):
-        usd_to_eur = CurrencyConversion.convert(base="USD", target="EUR", amount=1)
-        eur_to_usd = CurrencyConversion.convert(base="EUR", target="USD", amount=1)
+        usd_to_eur = CurrencyConversion.convert(base_currency="USD", target_currency="EUR", amount=1)
+        eur_to_usd = CurrencyConversion.convert(base_currency="EUR", target_currency="USD", amount=1)
 
         self.assertEqual(usd_to_eur, 1)
         self.assertEqual(eur_to_usd, 1)
 
     def testConversionWithObjects(self):
-        eur_to_chf = CurrencyConversion.convert(base=self.EUR, target=self.CHF, amount=1)
-        chf_to_eur = CurrencyConversion.convert(base=self.CHF, target=self.EUR, amount=1)
+        eur_to_chf = CurrencyConversion.convert(base_currency=self.EUR, target_currency=self.CHF, amount=1)
+        chf_to_eur = CurrencyConversion.convert(base_currency=self.CHF, target_currency=self.EUR, amount=1)
 
         self.assertEqual(eur_to_chf, 2)
         self.assertEqual(chf_to_eur, 0.5)
 
     def testConversionFromConversionObjectWithString(self):
-        eur_to_chf = self.EUR_TO_CHF.convert_to(target="CHF", amount=1)
+        eur_to_chf = self.EUR_TO_CHF.convert_to(target_currency="CHF", amount=1)
 
         self.assertEqual(eur_to_chf, 2)
 
     def testConversionFromConversionObjectWithObject(self):
-        eur_to_chf = self.EUR_TO_CHF.convert_to(target=self.CHF, amount=1)
+        eur_to_chf = self.EUR_TO_CHF.convert_to(target_currency=self.CHF, amount=1)
 
         self.assertEqual(eur_to_chf, 2)
 
     def testConversionWithNewerReverseConversionTimestamp(self):
         date_yesterday = timezone.now() - timedelta(days=1)
 
-        CurrencyConversion.objects.create(base=self.EUR, target=self.USD, multiplier=2, timestamp=date_yesterday)
-        CurrencyConversion.objects.create(base=self.USD, target=self.EUR, multiplier=3)
+        CurrencyConversion.objects.create(base_currency=self.EUR, target_currency=self.USD, multiplier=2, timestamp=date_yesterday)
+        CurrencyConversion.objects.create(base_currency=self.USD, target_currency=self.EUR, multiplier=3)
 
-        eur_to_usd = CurrencyConversion.convert(base="EUR", target="USD", amount=1)
+        eur_to_usd = CurrencyConversion.convert(base_currency="EUR", target_currency="USD", amount=1)
 
         self.assertAlmostEqual(eur_to_usd, Decimal(0.33), places=2)
